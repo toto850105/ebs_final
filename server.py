6d2c0dd1550d6fc83f9e73079215ee70eb7a61e6
HRY <toto850105@gmail.com>
2017-05-25 21:07:08 +0800

    add post

--- server.py
+++ server.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("hello.html")
 
+@app.route("/post", methods=['POST'])
+def post():
+    data = request.form['data']
+    print(data)
+    return 'You POSTED!'
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
